2ba6e0f8a2939296a386f099c9408b6116ba491f
khashaaaa <forwardent01@gmail.com>
2025-02-17 18:17:36 +0800

    Change

--- src/routes/user_router.rs
+++ src/routes/user_router.rs
@@ -3,11 +3,11 @@ use crate::handlers::user_handler::{create_user, get_users, get_user, update_use
 
 pub fn user_router(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/")
-            .route("", web::get().to(get_users))
-            .route("/create", web::post().to(create_user))
-            .route("/{id}", web::get().to(get_user))
-            .route("/{id}/update", web::put().to(update_user))
-            .route("/{id}/delete", web::delete().to(delete_user)),
+        web::scope("")
+            .route("/", web::get().to(get_users)) 
+            .route("create", web::post().to(create_user)) 
+            .route("{id}", web::get().to(get_user)) 
+            .route("{id}/update", web::put().to(update_user)) 
+            .route("{id}/delete", web::delete().to(delete_user)),
     );
 }
